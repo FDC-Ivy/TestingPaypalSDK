b318d0caf3722968a2527d2aa12efc23638c904f
FDC-Ivy <109194792+FDC-Ivy@users.noreply.github.com>
2023-06-05 10:21:10 +0800

    Add

---
+++ app/src/main/java/com/example/testingpaypalsdk/Config/Config.java
@@ -0,0 +1,5 @@
+package com.example.testingpaypalsdk.Config;
+
+public class Config {
+    public static final String PAYPAL_CLIENT_ID = "AVG2wV_V4lYSK9L0ZMPkBUYvOyDR5NSzZWcdNQSkeNclULewFPZEzqG3QM4hwfoyfQjZ9rsjr5yLYkzS";
+}
